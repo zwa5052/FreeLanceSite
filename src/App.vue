a88936ef6420528b24beaf1881dcaaa22fcd8562
Zachary Auker <zwa5052@gmail.com>
2022-05-30 11:52:46 -0400

    Initial commit of vue 3 vite application and bulma css library

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import navbar from './components/NavBar.vue'
+</script>
+
+<template>
+  <header>
+    <navbar></navbar>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import '@/assets/base.css';
+
+
+</style>
